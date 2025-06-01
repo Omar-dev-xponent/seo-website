e96ef2ed1de16d9fb209bb960211da66ff500544
Omar-dev-xponent <omar12.xponent@gmail.com>
2025-06-01 17:58:56 +0600

    style: testimonial section

---
+++ app/(site)/site-database/_components/testimonials.tsx
@@ -0,0 +1,20 @@
+import TestimonialCard from "@/components/testimonials";
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <section>
+      <div className="app-container">
+        <h4 className="text-2xl leading-[1.4] sm:text-3xl sm:leading-10 md:text-5xl font-semibold text-center text-typography-100 md:leading-[62px] font-display">
+          What <span className="text-brand-100">users</span> are saying
+        </h4>
+        <p className="mt-6 text-xl text-center font-display text-typography-50">
+          Discover how we’ve helped businesses like yours achieve success
+        </p>
+      </div>
+      <TestimonialCard />
+    </section>
+  );
+};
+
+export default Testimonials;
--- app/(site)/site-database/page.tsx
+++ app/(site)/site-database/page.tsx
@@ -3,12 +3,14 @@ import SiteDbHeroSection from "@/app/(site)/site-database/_components/hero";
 import BrandSection from "@/components/brand";
 import SiteDbFeature from "@/app/(site)/site-database/_components/feature";
 import TrialCTA from "@/components/trial-cta";
+import Testimonials from "@/app/(site)/site-database/_components/testimonials";
 const SiteDatabase = () => {
   return (
     <>
       <SiteDbHeroSection />
       <BrandSection />
       <SiteDbFeature />
+      <Testimonials />
       <TrialCTA
         title={`Build better backlinks. Faster.`}
         description={`Start exploring our site database and launch smarter \n campaigns in minutes.`}
