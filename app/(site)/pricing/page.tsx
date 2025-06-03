43a00da24ca1f06f916afea30a6695e0d2580c1d
Omar-dev-xponent <omar12.xponent@gmail.com>
2025-06-03 17:53:05 +0600

    style: pricing page

--- app/(site)/pricing/page.tsx
+++ app/(site)/pricing/page.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Hero from "@/app/(site)/pricing/_components/hero";
+import TrialCTA from "@/components/trial-cta";
 
 const Pricing = () => {
   return (
     <>
       <Hero />
+      <TrialCTA
+        title={`Lock in 30% off for 3 months. \n limited time only!`}
+        description={`Start risk-free with free migration assistance and a 30-day \n money-back guarantee`}
+      />
     </>
   );
 };
