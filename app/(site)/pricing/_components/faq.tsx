63b0b1be567fa945cc46eb563344984bd76e7ce5
Omar-dev-xponent <omar12.xponent@gmail.com>
2025-06-11 13:23:06 +0600

    style: pricing page gradient

--- app/(site)/pricing/_components/faq.tsx
+++ app/(site)/pricing/_components/faq.tsx
@@ -1,3 +1,4 @@
+import AquaCircleSVG from "@/components/svg-defs/AquaCircleSVG";
 import {
   Accordion,
   AccordionContent,
@@ -34,36 +35,41 @@ const Faq = () => {
     },
   ];
   return (
-    <section className="w-full max-w-3xl px-5 pt-8 mx-auto md:px-6 2xl:px-0 md:pt-24 sm:pt-12">
-      <h2 className="mb-4 text-2xl font-semibold text-center sm:text-4xl font-display text-typography-100 sm:mb-7">
-        Frequently Asked Question
-      </h2>
-      <p className="text-base text-center font-display text-typography-50">
-        Quickly discover the best domains tailored to your keywords.
-      </p>
-      <Accordion type="single" collapsible className="mt-6 sm:mt-12">
-        {faqContent.map((item, ind) => {
-          return (
-            <AccordionItem
-              value={`item-${item.id}`}
-              className="border-b-[1px] last:border-0 border-typography-25"
-            >
-              <AccordionTrigger
-                iconVisible={false}
-                className="font-medium text-left border-none text-md sm:text-lg group text-typography-100 font-display hover:no-underline"
+    <section className="relative z-10">
+      <div className="w-full max-w-3xl px-5 pt-8 mx-auto md:px-6 2xl:px-0 md:pt-24 sm:pt-12">
+        <h2 className="mb-4 text-2xl font-semibold text-center sm:text-4xl font-display text-typography-100 sm:mb-7">
+          Frequently Asked Question
+        </h2>
+        <p className="text-base text-center font-display text-typography-50">
+          Quickly discover the best domains tailored to your keywords.
+        </p>
+        <Accordion type="single" collapsible className="mt-6 sm:mt-12">
+          {faqContent.map((item, ind) => {
+            return (
+              <AccordionItem
+                value={`item-${item.id}`}
+                className="border-b-[1px] last:border-0 border-typography-25"
               >
-                <span>{item.question}</span>{" "}
-                <span className="p-2 sm:p-3 rounded-full transition-colors bg-typography-10 group-data-[state=open]:bg-typography-100">
-                  <HiOutlineArrowDown className="text-base transition-transform group-data-[state=open]:rotate-180 group-data-[state=open]:text-white" />
-                </span>
-              </AccordionTrigger>
-              <AccordionContent className="text-sm sm:text-base w-full sm:max-w-[75%] text-typography-50 font-display">
-                {item.answer}
-              </AccordionContent>
-            </AccordionItem>
-          );
-        })}
-      </Accordion>
+                <AccordionTrigger
+                  iconVisible={false}
+                  className="font-medium text-left border-none text-md sm:text-lg group text-typography-100 font-display hover:no-underline"
+                >
+                  <span>{item.question}</span>{" "}
+                  <span className="p-2 sm:p-3 rounded-full transition-colors bg-typography-10 group-data-[state=open]:bg-typography-100">
+                    <HiOutlineArrowDown className="text-base transition-transform group-data-[state=open]:rotate-180 group-data-[state=open]:text-white" />
+                  </span>
+                </AccordionTrigger>
+                <AccordionContent className="text-sm sm:text-base w-full sm:max-w-[75%] text-typography-50 font-display">
+                  {item.answer}
+                </AccordionContent>
+              </AccordionItem>
+            );
+          })}
+        </Accordion>
+      </div>
+      <div className="absolute top-0 hidden pointer-events-none xl:block -z-10 -left-10 lg:-top-20 lg:-left-16">
+        <AquaCircleSVG className="w-full h-full lg:max-w-[577px] lg:h-[741px]" />
+      </div>
     </section>
   );
 };
--- app/(site)/pricing/_components/hero.tsx
+++ app/(site)/pricing/_components/hero.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import PricingCard from "@/app/(site)/pricing/_components/pricing-card";
+import AquaCircleSVG from "@/components/svg-defs/AquaCircleSVG";
 const Hero = () => {
   const planDetails = [
     {
@@ -65,7 +66,7 @@ const Hero = () => {
     },
   ];
   return (
-    <section className="relative w-full bg-white">
+    <section className="relative z-10 w-full bg-white">
       <div className="relative flex flex-col items-center justify-center w-full pt-10 sm:pt-16 md:pt-24 app-container">
         <h1 className="sm:text-4xl md:text-6xl text-3xl font-semibold leading-10 sm:leading-[48px] sm:font-bold text-center md:leading-[80px] font-display text-typography-100">
           Clear, <span className="text-brand-100">simple pricing </span> built
@@ -111,6 +112,15 @@ const Hero = () => {
             })}
           </TabsContent>
         </Tabs>
+      </div>
+      <div className="absolute pointer-events-none -z-10 top-10 -left-10 lg:-top-20 lg:-left-16">
+        <AquaCircleSVG className="w-full h-full lg:max-w-[577px] lg:h-[741px]" />
+      </div>
+      <div className="-z-10 absolute -top-24 lg:-top-52 right-0 scale-x-[-1] pointer-events-none">
+        <AquaCircleSVG className="w-full h-full lg:max-w-[577px] lg:h-[741px]" />
+      </div>
+      <div className="-z-10 absolute -bottom-80 xl:block hidden  right-0 scale-x-[-1] pointer-events-none">
+        <AquaCircleSVG className="w-full h-full lg:max-w-[577px] lg:h-[741px]" />
       </div>
     </section>
   );
